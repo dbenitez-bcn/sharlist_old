1a7a571094ba7ea86fe1f4f931bf5fd53da6d25d
David Benitez Riba <davidbenitezriba@Eonar.local>
2018-11-01 14:44:11 +0100

    Setup DONE

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,122 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_i18n/flutter_i18n_delegate.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  Future<bool> isFirstTime() async{
+    final prefs = await SharedPreferences.getInstance();
+    final firstTime = prefs.getBool('welcome') !=null ? true: false;
+    if(!firstTime)prefs.setBool('welcome', true);
+    print("dentro del metodo $firstTime");
+    return firstTime;
+  }
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Onlist',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: _myHome(context),
+      localizationsDelegates: [
+        FlutterI18nDelegate(false, 'es'),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
     );
+  }
+
+  Widget _myHome(BuildContext context){
+    return new FutureBuilder<SharedPreferences>(
+      future: SharedPreferences.getInstance(),
+      builder:
+          (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return _loading(context);
+          default:
+            if (!snapshot.hasError) {
+        return snapshot.data.getBool("welcome") != null
+        ? new Text("Second Time")
+            : Text("First Time");
+        } else {
+        return new Text("Error :(");
+        }
+      }
+      },
+    );
+  }
+
+  Widget _loading(BuildContext context){
+  return Scaffold(
+    body: Center(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width*0.5,
+        height: MediaQuery.of(context).size.width*0.5,
+        child: CircularProgressIndicator(strokeWidth: 10.0,),
+      ),
+    ),
+  );
   }
 }
 
+
+
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  MyHomeState createState() => new MyHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MyHomeState extends State<MyHomePage> {
 
-  void _incrementCounter() {
+  String currentLang = 'en';
+
+  switchLang() {
     setState(() {
-      _counter++;
+      currentLang = currentLang == 'en' ? 'es' : 'en';
     });
+  }
+
+  void changeLang(BuildContext context)async{
+    print("tu sabe");
+    await FlutterI18n.refresh(context, 'es');
   }
 
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      changeLang(context);
+    });
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar:
+      new AppBar(title: new Text(FlutterI18n.translate(context, "title"))),
+      body: new Builder(builder: (BuildContext context) {
+        return new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(FlutterI18n.translate(context, "label.main",
+                  Map.fromIterables(["user"], ["Flutter lover"]))),
+              new FlatButton(
+                  onPressed: () async {
+                    switchLang();
+                    await FlutterI18n.refresh(context, currentLang);
+                    Scaffold.of(context).showSnackBar(new SnackBar(
+                      content: new Text(
+                          FlutterI18n.translate(context, "toastMessage")),
+                    ));
+                  },
+                  child: new Text(
+                      FlutterI18n.translate(context, "button.clickMe")))
+            ],
+          ),
+        );
+      }),
     );
   }
 }
